069d5585af20f3eb98b8aa356fa0bb1b282b24b1
PascalPieper <79706631+PascalPieper@users.noreply.github.com>
2021-12-30 19:31:22 +0100

    Did more stuff

--- app/src/main/java/com/example/portfolio1/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/portfolio1/viewModel/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.example.portfolio1.viewModel
 import android.app.Application
 import android.widget.Toast
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
@@ -13,15 +14,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.DefaultAlpha
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
+import com.example.portfolio1.database.entities.Picture
+import com.example.portfolio1.database.entities.Result
 import com.example.portfolio1.database.entities.Welcome
 import com.example.portfolio1.webAPI.ktorHttpClient
 import com.example.portfolio1.webAPI.randomUserAPI
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 
@@ -38,9 +43,10 @@ class MainViewModel (application: Application) : AndroidViewModel(application)
            internalUser.postValue("")
            val users = api.get(count)
            users.results.forEach(){
-               val test = it.name
+
            }
-           //internalUser.postValue(Json.encodeToString(Welcome.serializer(), users))
+           internalUser.postValue(Json.encodeToString(Welcome.serializer(), users))
+           internalUser.postValue(users.results[0].name.first)
        }
    }
 
@@ -70,11 +76,15 @@ class MainViewModel (application: Application) : AndroidViewModel(application)
 
     }
 
-//    @Composable
-//    fun DisplayUser(user: Welcome){
-//    Image("profile_picture", Modifier, Alignment.CenterStart)
-//    Text(text = name)
-//    }
+    @Composable
+    fun DisplayUser(user: Result){
+
+    ImageView(
+
+        //Picasso.get().load(user.picture.medium).into(imageView)
+    )
+    Text(text = name)
+   }
 
 
 }
