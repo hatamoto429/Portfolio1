d130e4ca050fdb514a173433113d18d418d07a71
PascalPieper <79706631+PascalPieper@users.noreply.github.com>
2022-01-05 20:21:25 +0100

    Made some Gradle changes

--- app/src/main/java/com/example/portfolio1/view/SettingsContent.kt
+++ app/src/main/java/com/example/portfolio1/view/SettingsContent.kt
@@ -8,9 +8,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,10 +21,7 @@ import com.example.portfolio1.viewModel.SettingsViewModel
 
 @Composable
 fun SettingsContent(navController: NavController, settingsViewModel: SettingsViewModel) {
-    var generationAmount: Int?
-fun SetCurrentInput(value: Int?){
-    generationAmount = value
-}
+
     Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -36,22 +31,46 @@ fun SetCurrentInput(value: Int?){
         var deleteButtonText = ("Delete All Users")
         var createButtonText = ("Generate All Users")
         val textState = remember { mutableStateOf(TextFieldValue()) }
-
+        var generationAmount: Int?
         var currentInput = ""
+        //val userGenCount by settingsViewModel.userGenCount.observeAsState()
+        var userGenCount by remember { mutableStateOf(10) }
 
         Text(
             text = title,
             modifier = Modifier.padding(10.dp)
         )
-
+        Text(
+            text = "Current amount of generated users: $userGenCount",
+            modifier = Modifier.padding(10.dp)
+        )
+        Text(
+            text = "Input the new number of users to generate:" ,
+            modifier = Modifier.padding(10.dp)
+        )
 
         TextField(value = textState.value, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             onValueChange = {
             textState.value = it
-            generationAmount = it.toString().toIntOrNull()
-            println(generationAmount)
-        })
+                currentInput = textState.value.text
+                generationAmount = currentInput.toIntOrNull()
+                if (generationAmount != null){
+                    userGenCount = generationAmount as Int
+                }
 
+        })
+        Button(
+
+            onClick = {},
+            modifier = Modifier.padding(top = 8.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green)
+
+
+        ) {
+            val buttonText = "Generate new users"
+            Text(text = buttonText)
+
+        }
         Button(
 
             onClick = {},
@@ -64,17 +83,7 @@ fun SetCurrentInput(value: Int?){
 
         }
 
-        Button(
 
-            onClick = {},
-            modifier = Modifier.padding(top = 8.dp),
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green)
-
-
-        ) {
-            //Text(text = buttonText)
-
-        }
     }
 
 }
--- app/src/main/java/com/example/portfolio1/viewModel/SettingsViewModel.kt
+++ app/src/main/java/com/example/portfolio1/viewModel/SettingsViewModel.kt
@@ -2,7 +2,10 @@ package com.example.portfolio1.viewModel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 
 class SettingsViewModel (application: Application) : AndroidViewModel(application) {
-
+    val currentGeneratedUserCount = MutableLiveData<Int?>(10)
+    val userGenCount: LiveData<Int?> = currentGeneratedUserCount
 }
