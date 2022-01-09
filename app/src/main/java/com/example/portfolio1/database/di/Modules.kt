94323b2458d365c0daff0bb1c17406a8a3e94610
PascalPieper <79706631+PascalPieper@users.noreply.github.com>
2022-01-09 21:39:52 +0100

    deleted all

--- app/src/main/java/com/example/portfolio1/database/di/Modules.kt
+++
@@ -1,5 +0,0 @@
-package com.example.portfolio1.database.di
-
-import org.koin.core.module.Module
-
-fun getApplicationModules() : List<Module> = listOf(networkModule, viewModelModule)
--- app/src/main/java/com/example/portfolio1/webAPI/NetworkSource.kt
+++
@@ -1,9 +0,0 @@
-package com.example.portfolio1.webAPI
-
-import com.example.portfolio1.database.entities.ApiUser
-
-interface NetworkSource {
-
-    suspend fun getUsers(resultCount: Int): ApiUser
-
-}
